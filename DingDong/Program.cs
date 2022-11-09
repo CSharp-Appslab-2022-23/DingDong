29c1495df83ef8defd42d57b9c8d2fef5a5b300f
Roman Popper <romanpopper@gmail.com>
2022-11-09 15:34:14 +0100

    Add solution do ding dong problem

--- DingDong/Program.cs
+++ DingDong/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DingDong
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] dingDongEnergies = Console.ReadLine().Split(' ');
+            var dingEnergy = int.Parse(dingDongEnergies[0]);
+            var dongEnergy = int.Parse(dingDongEnergies[1]);
+            string monsterShout = Console.ReadLine().ToUpper();
+
+            var totalEnergyUsed = 0;
+
+            var alreadyProcessedChars = new List<char>();
+            foreach (char c in monsterShout)
+            {
+                if (c == 'A' || alreadyProcessedChars.Contains(c))
+                {
+                    continue;
+                }
+
+                alreadyProcessedChars.Add(c);
+
+                var cCount = 0;
+                foreach (char t in monsterShout)
+                {
+                    if (c == t)
+                    {
+                        cCount++;
+                    }
+                }
+
+                int cDingPrice = cCount * dingEnergy;
+                int cDongPrice = ((int)c - (int)'A') * dongEnergy;
+
+                totalEnergyUsed += Math.Min(cDingPrice, cDongPrice);
+            }
+
+            Console.WriteLine(totalEnergyUsed);
         }
     }
 }
